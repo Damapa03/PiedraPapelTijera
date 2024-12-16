cc7890c061db7e56e7352e5e5c947cf250c42022
Daniel Marin <damapa2003@gmail.com>
2024-12-16 10:56:14 +0100

    Fin

---
+++ PiedraPapelPistola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using PiedraPapelPistola;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Boolean flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Seleccione modo de juego: \n1. Partida normal \n2.Torneo \n3. Salir");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Torneo.Bracket();
+                    break;
+                case 2:
+                    Partida.Game();
+                    break;
+                case 3:
+                    flag = false;
+                    break;
+            }
+        }
+    }
+}
